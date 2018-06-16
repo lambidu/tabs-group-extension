2b2aaf6f51f30e7cec0b1546e497eae11b01733a
Roman Popovici <contact@lambidu.com>
2018-06-16 03:13:56 +0300

    Fix: Add missed polyfill

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -45,6 +45,8 @@
 // Used for reflect-metadata in JIT. If you use AOT (and only Angular decorators), you can remove.
 import 'core-js/es7/reflect';
 
+// WebExtension
+import 'webextension-polyfill';
 
 /**
  * Web Animations `@angular/platform-browser/animations`
